9995192fd87568044c605f8da754eb00831dbbc2
nrymarz <nrymarz@gmail.com>
2020-10-29 21:50:32 -0500

    commit tree bf777b6d5aaeac608aac656d97b5b178480d0565

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+  class TriangleError < StandardError
+  end
+  def initialize(num1,num2,num3)
+    @s1 = num1
+    @s2 = num2
+    @s3 = num3
+  end
+  def kind
+    sides = [@s1,@s2,@s3].sort
+    if sides[2]<sides[0]+sides[1] && sides[0]>0
+      if sides[0] == sides[2]
+        :equilateral
+      elsif sides[0] == sides[1] || sides[1]==sides[2]
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
 end
